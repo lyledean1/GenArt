d755704d679eae155949d67971f1311dc08356d8
D0naldSh1m0da <conchubhar.gannon@gmail.com>
2018-11-16 16:51:33 +0000

    scraper

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"GenArt/generative"
+	"flag"
 	"github.com/sirupsen/logrus"
+	"image"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -12,6 +15,20 @@ func main() {
 		log.Fatal(err)
 	}
 
+	generateImage(img, err)
+
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+
+}
+
+func generateImage(img image.Image, err error) {
 	// Crop attempts to find the best crop of img based on the given width and height values.
 	img, err = generative.Crop(img, 1000, 1000)
 	if err != nil {
@@ -21,34 +38,28 @@ func main() {
 	if err != nil {
 		logrus.Error("unable to save mage " + err.Error())
 	}
-
 	img, err = generative.OpenImage("images/cropped.jpg")
 	if err != nil {
 		logrus.Error("unable to open cropped image " + err.Error())
 	}
-
 	sat := generative.Saturate(img)
 	err = generative.SaveImage(sat, ".", "images/saturated.jpg")
 	if err != nil {
 		logrus.Error("unable to save saturated image " + err.Error())
 	}
-
 	mult := generative.Multiply(img)
 	err = generative.SaveImage(mult, ".", "images/multiplied.jpg")
 	if err != nil {
 		logrus.Error("unable to save multiplied image " + err.Error())
 	}
-
 	shrp := generative.Sharpen(sat)
 	err = generative.SaveImage(shrp, ".", "images/sharpened.jpg")
 	if err != nil {
 		logrus.Error("unable to save sharpened image " + err.Error())
 	}
-
 	pri := generative.PrimitivePicture(sat)
 	err = generative.SaveImage(pri, ".", "images/primitive.jpg")
 	if err != nil {
 		logrus.Error("unable to save primitive image " + err.Error())
 	}
-
 }
---
+++ scraper/main.go
@@ -0,0 +1,66 @@
+// find_images_in_page.go
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+}
+
+func getImagesFromUrl(url string) ([]string) {
+
+	var urls []string
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	// Find and print image URLs
+	document.Find("img").Each(func(index int, element *goquery.Selection) {
+		imgSrc, exists := element.Attr("src")
+		if exists && strings.Contains(imgSrc, ".jpg") {
+			urls = append(urls, imgSrc)
+		}
+	})
+
+	return urls
+}
+
+func saveImage(url string) {
+	// don't worry about errors
+	response, e := http.Get(url)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer response.Body.Close()
+
+	//open a file for writing
+	file, err := os.Create("/tmp/asdf.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Use io.Copy to just dump the response body to the file. This supports huge files
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Success!")
+}
