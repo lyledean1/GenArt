1d14c03c99baf0605e675fd62be2ec23754cc233
D0naldSh1m0da <conchubhar.gannon@gmail.com>
2018-11-19 21:47:13 +0000

    scraper

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"github.com/azer/go-flickr"
 	"github.com/jasonlvhit/gocron"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -19,19 +20,25 @@ func main() {
 
 	go cron(client)
 
-	port := flag.String("p", "8100", "port to serve on")
+	port := flag.String("p", "80", "port to serve on")
 	directory := flag.String("d", ".", "the directory of static file to host")
 	flag.Parse()
 
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.HandleFunc("/", NoraHandler)
+
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 
 }
 
+func NoraHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Nora is an amazing woman")
+}
+
 func cron(client flickr.Client) {
 	gocron.Every(1).Minute().Do(scrape.ScrapeFlickr, client, "trees")
-	gocron.Every(2).Minutes().Do(generative.GenerateImage)
+	gocron.Every(1).Minute().Do(generative.GenerateImage)
 	<-gocron.Start()
 }
